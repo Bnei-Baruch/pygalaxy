3cb9adf7513ab1c8789fa80bfcabe1b07eb28a4a
Edo Shor <edo.shor@pipl.com>
2016-05-24 05:05:54 +0300

    PEP8 + remove unused code.

--- gst_dec/gst_sdi3.py
+++ gst_dec/gst_sdi3.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from multiprocessing import Process, Queue
 import gi
+
 gi.require_version('Gst', '1.0')
 from gi.repository import GObject, Gst
-from time import sleep,time,localtime
+from time import sleep, time, localtime
 from socket import socket
 
 import sys
@@ -12,36 +13,32 @@ import os
 script_name = os.path.basename(__file__)
 script_name = script_name.split(".")[0]
 
-log_path = 'logs' 
+log_path = 'logs'
 if not os.path.exists(log_path):
     os.makedirs(log_path)
 
-def first_zero(num,cnt=2):
+
+def first_zero(num, cnt=2):
     num = str(num)
-    while len(num)<cnt:
-        num = '0'+num
+    while len(num) < cnt:
+        num = '0' + num
     return num
+
 
 def tt_log():
     ttuple = localtime(time())
-    return str(ttuple.tm_year)+first_zero(ttuple.tm_mon)+first_zero(ttuple.tm_mday)+"_"+first_zero(ttuple.tm_hour)+":"+first_zero(ttuple.tm_min)+":"+first_zero(ttuple.tm_sec)
+    return str(ttuple.tm_year) + first_zero(ttuple.tm_mon) + first_zero(ttuple.tm_mday) + "_" + first_zero(
+        ttuple.tm_hour) + ":" + first_zero(ttuple.tm_min) + ":" + first_zero(ttuple.tm_sec)
 
-sys.stdout = open(log_path+"/"+script_name+"_"+tt_log()+".log","w",0)
 
-
+sys.stdout = open(log_path + "/" + script_name + "_" + tt_log() + ".log", "w", 0)
 
 global pipeline
-global f
-f = "ffff"
 global overlay
 loop = GObject.MainLoop()
 context = loop.get_context()
 GObject.threads_init()
 Gst.init(None)
-pipeline = None
-
-#udpsrc port=8024 caps="application/x-rtp, media=video, payload=96, clock-rate=90000, encoding-name=VP8" ! rtpjitterbuffer do-lost=false latency=50 drop-on-latency=true ! rtpvp8depay ! vp8dec ! queue ! videoconvert ! videoscale add-borders=false method=4 ! videorate ! interlace field-pattern=2:2 ! video/x-raw, format=UYVY, width=720, height=576, framerate=25/1, interlace-mode=interleaved, pixel-aspect-ratio=12/11, colorimetry=bt601, chroma-site=mpeg2 ! textoverlay name=tittle1 ypad=10 xpad=10 font-desc="LiberationSerif 35px" draw-shadow=false shaded-background=false valignment=top halignment=right text=" " ! textoverlay name=tittle2 ypad=10 xpad=360 font-desc="LiberationSerif 35px" draw-shadow=false shaded-background=false valignment=top halignment=right text=" " ! textoverlay name=tittle3 ypad=280 xpad=360 font-desc="LiberationSerif 35px" draw-shadow=false shaded-background=false valignment=top halignment=right text=" " ! textoverlay name=tittle4 ypad=280 xpad=10 font-desc="LiberationSerif 35px" draw-shadow=false shaded-background=false valignment=top halignment=right text=" " ! compositor name=mix timeout=100000000 ! decklinkvideosink device-number=2 mode=3
-
 pipeline = Gst.parse_launch('''
 udpsrc timeout=1000000000 port=8024 caps="application/x-rtp, media=video, payload=96, clock-rate=90000, encoding-name=VP8-DRAFT-IETF-01" ! 
 rtpjitterbuffer do-lost=false latency=50 drop-on-latency=true ! 
@@ -59,110 +56,100 @@ textoverlay name=tittle3 ypad=280 xpad=360 font-desc="LiberationSerif 35px" draw
 textoverlay name=tittle4 ypad=280 xpad=10 font-desc="LiberationSerif 35px" draw-shadow=false shaded-background=false valignment=top halignment=right text="" !
 compositor name=mix timeout=100000000 !
 decklinkvideosink device-number=2 mode=3''')
+
 pipeline.set_state(Gst.State.PLAYING)
 sleep(3)
 bus = pipeline.get_bus()
-#bus.add_signal_watch()
-overlay_dict = {}
-#overlay_dict['8004'] = pipeline.get_by_name('overlay')
-overlay_dict['5024'] = pipeline.get_by_name('tittle2')
-overlay_dict['5026'] = pipeline.get_by_name('tittle3')
-overlay_dict['5028'] = pipeline.get_by_name('tittle1')
-overlay_dict['5030'] = pipeline.get_by_name('tittle4')
+overlay_dict = {
+    '5024': pipeline.get_by_name('tittle2'),
+    '5026': pipeline.get_by_name('tittle3'),
+    '5028': pipeline.get_by_name('tittle1'),
+    '5030': pipeline.get_by_name('tittle4')
+}
+
 global q
 q = Queue()
-
 
 
 def recieve_title_from_socket():
     host = 'localhost'
     port = 9090
-    client_id = '5024,5026,5028,5030'#'8004'
+    client_id = '5024,5026,5028,5030'
     global q
     while 1:
         try:
             sock = socket()
-            sock.connect((host,port))
-            sock.send(client_id+'#')
-            print tt_log()+" [recieve_title_from_socket] socket connected"
+            sock.connect((host, port))
+            sock.send(client_id + '#')
+            print tt_log() + " [recieve_title_from_socket] socket connected"
             break
         except:
-            print tt_log()+" [recieve_title_from_socket] socket not connected"
+            print tt_log() + " [recieve_title_from_socket] socket not connected"
             sleep(3)
-            
+
     while 1:
-        print tt_log()+" [recieve_title_from_socket] start cycle"
+        print tt_log() + " [recieve_title_from_socket] start cycle"
         rcv = sock.recv(1024)
         if rcv == '':
-           while 1:
-               print tt_log()+' [recieve_title_from_socket] socket try reconnect'
-               try:
-                   sock = socket()
-                   sock.connect((host,port))
-                   sock.send(client_id+'#')
-                   print tt_log()+' [recieve_title_from_socket] socket reconnected'
-                   break
-               except:
-                   print tt_log()+" [recieve_title_from_socket] can not reconnect" 
-               sleep(3)
-           
-        else:
-            print tt_log()+" [recieve_title] recieved: "+rcv
+            while 1:
+                print tt_log() + ' [recieve_title_from_socket] socket try reconnect'
+                try:
+                    sock = socket()
+                    sock.connect((host, port))
+                    sock.send(client_id + '#')
+                    print tt_log() + ' [recieve_title_from_socket] socket reconnected'
+                    break
+                except:
+                    print tt_log() + " [recieve_title_from_socket] can not reconnect"
+                sleep(3)
 
-            while rcv.count("#")>1:
-                rcv = rcv[rcv.find("#")+1:]
+        else:
+            print tt_log() + " [recieve_title] recieved: " + rcv
+
+            while rcv.count("#") > 1:
+                rcv = rcv[rcv.find("#") + 1:]
             title = rcv[:rcv.find("#")]
             q.put(title)
-            
-            print tt_log()+" [recieve_title] New title puted in Q: "+title
-    
-l = Process(target=recieve_title_from_socket)
-l.start()   
 
-#noStream = 2
+            print tt_log() + " [recieve_title] New title puted in Q: " + title
+
+
+l = Process(target=recieve_title_from_socket)
+l.start()
 start_after_timeout = 0
-### main loop
 while 1:
     sleep(0.01)
     try:
-        ggg = bus.poll(Gst.MessageType.ELEMENT,10) #type Gst.Message
-        #print "SOURCE: ",ggg.src
-        #print ggg
+        ggg = bus.poll(Gst.MessageType.ELEMENT, 10)  # type Gst.Message
         struct = ggg.get_structure()
         name33 = struct.get_name()
-        if (name33 == 'GstUDPSrcTimeout'):# or name33 == 'GstMessageQOS'):
-            #print "SOURCE: ",ggg.src.get_name() # type Gst.Element
-            #print "TYPE: ", ggg.type
+        if name33 == 'GstUDPSrcTimeout':  # or name33 == 'GstMessageQOS':
             tt_last_timeout = time()
-            if start_after_timeout==0:
-                print tt_log()+" [main loop] Element: ", ggg.src.get_name(),", Message: ", name33
+            if start_after_timeout == 0:
+                print tt_log() + " [main loop] Element: ", ggg.src.get_name(), ", Message: ", name33
                 start_after_timeout = 1
                 pipeline.set_state(Gst.State.READY)
                 pipeline.set_state(Gst.State.PAUSED)
                 pipeline.set_state(Gst.State.PLAYING)
-                print tt_log()+" [main loop] Restarting when timeouts starts"
-
-            #print ggg.src.get_state()
+                print tt_log() + " [main loop] Restarting when timeouts starts"
     except:
-        pass#print "NO MESSAGES"
-    if start_after_timeout and time()-tt_last_timeout>3:
-        print tt_log()+" [main loop] Restarting after timeouts"
+        pass  # no messages
+    if start_after_timeout and time() - tt_last_timeout > 3:
+        print tt_log() + " [main loop] Restarting after timeouts"
         pipeline.set_state(Gst.State.READY)
         pipeline.set_state(Gst.State.PAUSED)
         pipeline.set_state(Gst.State.PLAYING)
         start_after_timeout = 0
     message = ''
     try:
-        message=q.get(False)
-        print tt_log()+" [main loop] recieved new message: "+message
+        message = q.get(False)
+        print tt_log() + " [main loop] Recieved new message: " + message
     except:
         pass
-    if message!='':
+    if message != '':
         port = message[:message.find(':')]
-        title = message[message.find(':')+1:]
+        title = message[message.find(':') + 1:]
         if overlay_dict.has_key(port):
-            overlay_dict[port].set_property('text', '<span foreground="white" background="blue">'+title+'</span>')
+            overlay_dict[port].set_property('text', '<span foreground="white" background="blue">' + title + '</span>')
             pipeline.set_state(Gst.State.PLAYING)
-        print tt_log()+' [main loop] updated new title: '+ message
-
-        
+        print tt_log() + ' [main loop] updated new title: ' + message
--- gst_dec/gst_web.py
+++ gst_dec/gst_web.py
@@ -1,82 +1,79 @@
 #!/usr/bin/env python
-from bottle import  run ,route, request#, Bottle
-#from bottle import *
-from multiprocessing import  Process, Manager, Queue
-from socket import socket
-from time import sleep,time,localtime
-
-import sys
 import os
+import sys
+from multiprocessing import Process, Queue
+from socket import socket
+from time import sleep, time, localtime
+
+from bottle import run, route, request
 
 script_name = os.path.basename(__file__)
 script_name = script_name.split(".")[0]
 
-log_path = 'logs' 
+log_path = 'logs'
 if not os.path.exists(log_path):
     os.makedirs(log_path)
 
-def first_zero(num,cnt=2):
+
+def first_zero(num, cnt=2):
     num = str(num)
-    while len(num)<cnt:
-        num = '0'+num
+    while len(num) < cnt:
+        num = '0' + num
     return num
+
 
 def tt_log():
     ttuple = localtime(time())
-    return str(ttuple.tm_year)+first_zero(ttuple.tm_mon)+first_zero(ttuple.tm_mday)+"_"+first_zero(ttuple.tm_hour)+":"+first_zero(ttuple.tm_min)+":"+first_zero(ttuple.tm_sec)
-
-sys.stdout = open(log_path+"/"+script_name+"_"+tt_log()+".log","w",0)
-
+    return str(ttuple.tm_year) + first_zero(ttuple.tm_mon) + first_zero(ttuple.tm_mday) + "_" + first_zero(
+        ttuple.tm_hour) + ":" + first_zero(ttuple.tm_min) + ":" + first_zero(ttuple.tm_sec)
 
 
+sys.stdout = open(log_path + "/" + script_name + "_" + tt_log() + ".log", "w", 0)
 
 global sock
+
+
 def soccet_connect():
     global sock
     sock = socket()
-    sock.connect(('localhost',9090))
+    sock.connect(('localhost', 9090))
     sock.send('server#')
+
+
 global q
 q = Queue()
+
 
 class myBottleClass:
     global q
     q = Queue()
-    #global sock_clients
+
     def __init__(self):
-        print tt_log()+" myBottleClass created"
+        print tt_log() + " myBottleClass created"
+
     @route('/title.php', method=['GET', 'OPTIONS'])
     def index():
-        err=0
+        err = 0
         try:
             title = request.query.title
-	    port = request.query.port
+            port = request.query.port
         except:
-            err=1
+            err = 1
             print "[myBottleClass][index] Unexpected exception in title and port getting:", sys.exc_info()[0]
             raise
         if err:
             return "Err"
         try:
-            port=int(port)
+            port = int(port)
         except:
-            print tt_log()+" [myBottleClass] Broken port, must be integer"
+            print tt_log() + " [myBottleClass] Broken port, must be integer"
             return "Err"
-        print(tt_log()+ " [myBottleClass] Recieve message for client "+str(port)+": "+title)
-        #print sock_clients
+        print(tt_log() + " [myBottleClass] Recieve message for client " + str(port) + ": " + title)
         if title != "favicon.ico":
-            #if port in sock_clients[0].keys():
-	        print(tt_log()+" [myBottleClass] try put send title")
-                #print sock_clients
-                #sock_clients[port][0]=send("#"+title+"#")
-                q.put(str(port)+':'+title)
-                #send_to_port(port, title)
-                print(tt_log()+" [myBottleClass] message sended")
-        return("For client: "+str(port)+" sended title: "+title)
-            
-        #print("gggggggggggggggggggg")
-	#print("gggggggggggggggggggg")
-        
+            print(tt_log() + " [myBottleClass] try put send title")
+            q.put(str(port) + ':' + title)
+            print(tt_log() + " [myBottleClass] message sended")
+        return ("For client: " + str(port) + " sended title: " + title)
 
     def myBottleRun(self):
         run(host='0.0.0.0', port=8081)
@@ -86,80 +83,45 @@ global h
 myBottlObject = myBottleClass()
 h = Process(target=myBottlObject.myBottleRun)
 h.start()
-#h.join()
-
-#def get_vals_loop():
-#    try:
-#        print "try Q"
-#        sleep(3)
-#        f = q.get(False)
-#        print "get from Q"
-#        print (f)
-#        
-#    except:
-#	pass
-        #print "nothing"
-#last_time_web_request = time()
 
 while 1:
     try:
         soccet_connect()
-        print tt_log()+" Connected to soccet_gstreamer_server"
+        print tt_log() + " Connected to soccet_gstreamer_server"
         break
     except:
-        print tt_log()+" Can't connect to soccet_gstreamer_server, maybe it isn't run"
+        print tt_log() + " Can't connect to soccet_gstreamer_server, maybe it isn't run"
     sleep(3)
-
-
 
 while 1:
     sleep(0.05)
-    #print  tt_log()+" [main loop] New iteration"
     if not h.is_alive():
-        print tt_log()+" [main_loop] Exception: Web Server Process is not alieve. Restart it."
+        print tt_log() + " [main_loop] Exception: Web Server Process is not alieve. Restart it."
         myBottlObject = myBottleClass()
         h = Process(target=myBottlObject.myBottleRun)
         h.start()
-#    if time()-last_time_web_request >600:
-#        print tt_log()+" [main_loop] Web Server Process didn't recieve messages 10 minutes. Maybe it is in bad state. Restart it."
-#        if h.is_alive():
-#            h.terminate()
-#            print  tt_log()+" [main loop] Web Server Process alieve. Terminate it."
-#        myBottlObject = myBottleClass()
-#        h = Process(target=myBottlObject.myBottleRun)
-#        h.start()
-#        print tt_log()+" [main_loop] Web Server Process restarted."
-#        last_time_web_request = time()
-        
+
     try:
-#        print "try Q"
-#         sleep(3)
         txt = q.get(False)
-#        last_time_web_request = time()
-        print tt_log()+" [main loop] get from Q: ",txt
-        sock.send(txt+'#')
-        print tt_log()+" [main loop] sended: "+txt+'#'
+        print tt_log() + " [main loop] get from Q: ", txt
+        sock.send(txt + '#')
+        print tt_log() + " [main loop] sended: " + txt + '#'
         replay = sock.recv(1024)
         if replay == '':
-            print tt_log()+" [main loop] socket try to reconnect"
+            print tt_log() + " [main loop] socket try to reconnect"
             while 1:
                 try:
                     soccet_connect()
-                    print tt_log()+" [main loop] socket reconnected"
+                    print tt_log() + " [main loop] socket reconnected"
                     sleep(3)
-                    sock.send(txt+"#")
+                    sock.send(txt + "#")
                     replay = sock.recv(1024)
-                    print tt_log()+" [main loop] message sended: "+txt
+                    print tt_log() + " [main loop] message sended: " + txt
                     break
                 except:
-                    print tt_log()+" [main loop] socket not reconnected"
-                sleep(3)                
+                    print tt_log() + " [main loop] socket not reconnected"
+                sleep(3)
         else:
-            print tt_log()+" [main loop] message sended: "+txt
-#        
+            print tt_log() + " [main loop] message sended: " + txt
     except:
-	pass
-        #print "nothing"
-        
-
-
+        pass
