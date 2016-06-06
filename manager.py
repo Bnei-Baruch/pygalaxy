e6ee9810777a55bf6223e44f2ec8eba675b9ceb3
Edo Shor <edo.shor@pipl.com>
2016-06-06 04:32:08 +0300

    App config + environment based streams

--- manager.py
+++ manager.py
@@ -2,41 +2,56 @@ import logging
 
 import gi
 
-
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
 
 log = logging.getLogger(__name__)
 
+
 class GstreamerManager(object):
     SINGLE_CMD = ' ! '.join([
-        'videotestsrc pattern={}',
-        'video/x-raw,width=400,height=300',
-        'textoverlay name=overlay ypad=0 font-desc="LiberationSerif 32px" shaded-background=false draw-shadow=false valignment=top halignment=right text=""',
-        'autovideosink'])
+        'udpsrc timeout=1000000000 port={port} caps="application/x-rtp, media=video, payload=100, clock-rate=90000, encoding-name=VP8-DRAFT-IETF-01"',
+        'rtpjitterbuffer do-lost=false latency=50 drop-on-latency=true',
+        'rtpvp8depay',
+        'vp8dec',
+        'queue',
+        'videoconvert',
+        'videoscale add-borders=false method=4',
+        'videorate',
+        'interlace field-pattern=2:2',
+        'video/x-raw, format=UYVY, width=720, height=576, framerate=25/1, interlace-mode=interleaved, pixel-aspect-ratio=12/11, colorimetry=bt601, chroma-site=mpeg2',
+        'textoverlay name=overlay text="" ypad=0 valignment=top halignment=right font-desc="LiberationSerif 65px" shaded-background=false draw-shadow=false',
+        'compositor name=mix timeout=100000000',
+        'decklinkvideosink device-number={device_number} mode=3'
+    ])
 
-    LARGE_CMD = SINGLE_CMD.format('9')
-    SMALL_CMD = SINGLE_CMD.format('snow')
-    CONTROL_CMD = SINGLE_CMD.format('ball')
+    LARGE_CMD = SINGLE_CMD.format(port=8004, device_number=3)
+    SMALL_CMD = SINGLE_CMD.format(port=8014, device_number=0)
+    CONTROL_CMD = SINGLE_CMD.format(port=8044, device_number=1)
 
     FOURS_CMD = ' ! '.join([
-        'compositor name=mix timeout=100000000 sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=0 sink_1::ypos=180 sink_2::xpos=320 sink_2::ypos=0 sink_3::xpos=320 sink_3::ypos=180 sync=false',
-        'textoverlay name=overlay1 ypad=0 xpad=330 text="" font-desc="LiberationSerif 32px" shaded-background=false draw-shadow=false valignment=top halignment=right',
-        'textoverlay name=overlay2 ypad=180 xpad=330 text="" font-desc="LiberationSerif 32px" shaded-background=false draw-shadow=false valignment=top halignment=right',
-        'textoverlay name=overlay3 ypad=0 xpad=10 text="" font-desc="LiberationSerif 32px" shaded-background=false draw-shadow=false valignment=top halignment=right',
-        'textoverlay name=overlay4 ypad=180 xpad=10 text="" font-desc="LiberationSerif 32px" shaded-background=false draw-shadow=false valignment=top halignment=right',
-        'autovideosink'
-    ]) + ' ' + ' '.join([
-        'videotestsrc pattern=0 ! mix.',
-        'videotestsrc pattern=snow ! mix.',
-        'videotestsrc pattern=spokes ! mix.',
-        'videotestsrc pattern=ball ! mix.'
+        'udpsrc timeout=1000000000 port=8024 caps="application/x-rtp, media=video, payload=96, clock-rate=90000, encoding-name=VP8-DRAFT-IETF-01"',
+        'rtpjitterbuffer do-lost=false latency=50 drop-on-latency=true',
+        'rtpvp8depay',
+        'vp8dec',
+        'queue',
+        'videoconvert',
+        'videoscale add-borders=false method=4',
+        'videorate',
+        'interlace field-pattern=2:2',
+        'video/x-raw, format=UYVY, width=720, height=576, framerate=25/1, interlace-mode=interleaved, pixel-aspect-ratio=12/11, colorimetry=bt601, chroma-site=mpeg2',
+        'textoverlay name=overlay1 text="" ypad=10 xpad=10 valignment=top halignment=right font-desc="LiberationSerif 35px" draw-shadow=false shaded-background=false',
+        'textoverlay name=overlay2 text="" ypad=10 xpad=360 valignment=top halignment=right font-desc="LiberationSerif 35px" draw-shadow=false shaded-background=false',
+        'textoverlay name=overlay3 text="" ypad=280 xpad=360 valignment=top halignment=right font-desc="LiberationSerif 35px" draw-shadow=false shaded-background=false',
+        'textoverlay name=overlay4 text="" ypad=280 xpad=10 valignment=top halignment=right font-desc="LiberationSerif 35px" draw-shadow=false shaded-background=false',
+        'compositor name=mix timeout=100000000',
+        'decklinkvideosink device-number=2 mode=3'
     ])
 
     FOURS_PORT_SLOT_MAP = {
-        5024: '1',
-        5026: '2',
-        5028: '3',
+        5024: '2',
+        5026: '3',
+        5028: '1',
         5030: '4',
     }
 
@@ -101,3 +116,28 @@ class GstreamerManager(object):
         # TODO: implement something useful here
         return True
 
+
+class GstreamerManagerDev(GstreamerManager):
+    SINGLE_CMD = ' ! '.join([
+        'videotestsrc pattern={}',
+        'video/x-raw,width=400,height=300',
+        'textoverlay name=overlay ypad=0 font-desc="LiberationSerif 32px" shaded-background=false draw-shadow=false valignment=top halignment=right text=""',
+        'autovideosink'])
+
+    LARGE_CMD = SINGLE_CMD.format('9')
+    SMALL_CMD = SINGLE_CMD.format('snow')
+    CONTROL_CMD = SINGLE_CMD.format('ball')
+
+    FOURS_CMD = ' ! '.join([
+        'compositor name=mix timeout=100000000 sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=0 sink_1::ypos=180 sink_2::xpos=320 sink_2::ypos=0 sink_3::xpos=320 sink_3::ypos=180 sync=false',
+        'textoverlay name=overlay1 ypad=0 xpad=330 text="" font-desc="LiberationSerif 32px" shaded-background=false draw-shadow=false valignment=top halignment=right',
+        'textoverlay name=overlay2 ypad=180 xpad=330 text="" font-desc="LiberationSerif 32px" shaded-background=false draw-shadow=false valignment=top halignment=right',
+        'textoverlay name=overlay3 ypad=0 xpad=10 text="" font-desc="LiberationSerif 32px" shaded-background=false draw-shadow=false valignment=top halignment=right',
+        'textoverlay name=overlay4 ypad=180 xpad=10 text="" font-desc="LiberationSerif 32px" shaded-background=false draw-shadow=false valignment=top halignment=right',
+        'autovideosink'
+    ]) + ' ' + ' '.join([
+        'videotestsrc pattern=0 ! mix.',
+        'videotestsrc pattern=snow ! mix.',
+        'videotestsrc pattern=spokes ! mix.',
+        'videotestsrc pattern=ball ! mix.'
+    ])
--- webapp.py
+++ webapp.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python
 import logging.config
+import os
+import pprint
 
-from bottle import run, post, request, response, get
+import bottle
+from bottle import request, response
 
-from manager import GstreamerManager
+from manager import GstreamerManager, GstreamerManagerDev
 
 logging.basicConfig(level=logging.DEBUG,
                     format='[%(asctime)s] %(levelname)s %(process)d %(threadName)s [%(name)s:%(lineno)s] %(message)s')
 
 log = logging.getLogger(__name__)
 
+app = bottle.default_app()
+app.config['host'] = os.environ.get('GALAXY_HOST', '0.0.0.0')
+app.config['port'] = int(os.environ.get('GALAXY_PORT', 8081))
+app.config['galaxy.env'] = os.environ.get('GALAXY_ENV', 'dev')
+
+log.debug('Application config')
+log.debug(pprint.pformat(app.config))
+
 log.info('Initializing Gstreamer Manager')
-GSTManager = GstreamerManager()
+GSTManager = GstreamerManagerDev() if app.config['galaxy.env'] == 'dev' else GstreamerManager()
 GSTManager.initialize()
 log.info('Gstreamer Manager initialization complete')
 
 
-@get('/titles/')
+@app.get('/titles/')
 def get_titles():
     return GSTManager.get_titles()
 
 
-@post('/titles/')
+@app.post('/titles/')
 def set_titles():
     log.debug(request.json)
     for x in request.json:
@@ -29,7 +40,7 @@ def set_titles():
     return "SUCCESS"
 
 
-@get('/health_check/')
+@app.get('/health_check/')
 def health_check():
     if not GSTManager.is_alive():
         response.status = '500 Gstreamer manager is dead'
@@ -39,4 +50,4 @@ def health_check():
 
 
 log.info('Running application')
-run(host='localhost', port=8081)
+app.run(host=app.config['host'], port=app.config['port'])
