b55785391ffa39fd43dda8bfe4c4ab599e4e6576
Edo Shor <edo.shor@pipl.com>
2016-06-25 06:30:27 +0300

    Add CORS headers

--- webapp.py
+++ webapp.py
@@ -28,6 +28,13 @@ GSTManager.initialize()
 log.info('Gstreamer Manager initialization complete')
 
 
+@app.hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+
 @app.get('/titles/')
 def get_titles():
     return GSTManager.get_titles()
